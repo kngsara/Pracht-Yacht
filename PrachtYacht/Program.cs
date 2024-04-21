95c21379b4cf3973f707943bc39cf0ad8de310ed
Sara <sporic@algebra.hr>
2024-04-21 16:46:10 +0200

    dodan neki bezvezni komentar

--- PrachtYacht/Program.cs
+++ PrachtYacht/Program.cs
@@ -14,6 +14,7 @@ namespace PrachtYacht
         [STAThread]
         static void Main()
         {
+            // neke bezveze
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Registration());
